ecc83ca308184870c23a2f2f0b1951367e85efd5
Stephen Fox <stephenfox995@hotmail.com>
2015-04-27 20:11:11 +0100

    Initial project setup.

---
+++ DIT/DIT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DIT
+//
+//  Created by Stephen Fox on 27/04/2015.
+//  Copyright (c) 2015 Stephen Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
